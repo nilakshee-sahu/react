90f32c85b006e3baa45246794d4f7faba69ec20a
nilakshee-sahu <sahunilakshee@gmail.com>
2026-05-14 12:12:38 +0530

    a question on setCounter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -11,7 +11,10 @@ function App() {
 
   const addValue = () => {
     if(counter < 20){
-      setCounter(counter+1);
+      // setCounter(counter+1);
+      setCounter(prevCounter => prevCounter+1);
+      setCounter(prevCounter => prevCounter+1);
+      setCounter(prevCounter => prevCounter+1);
     }
     // counter = counter + 1;
     // setCounter(counter);
